882036917ca13a747fed56e0c690f4a296ad0146
SwarnaSiddha <siddhaswarnajyothi@gmail.com>
2026-05-09 19:52:20 +0530

    certiwipe

--- electron.js
+++ electron.js
@@ -1,43 +1,44 @@
-// const { app, BrowserWindow } = require("electron");
-// const path = require("path");
-
-// let mainWindow;
-
-// function createWindow() {
-//   mainWindow = new BrowserWindow({
-//     width: 1400,
-//     height: 900,
-//     webPreferences: {
-//       nodeIntegration: false,
-//     },
-//   });
-
-//   mainWindow.loadFile(
-//     path.join(__dirname, "frontend", "dist", "index.html")
-//   );
-// }
-
-// app.whenReady().then(() => {
-//   createWindow();
-// });
-
-// app.on("window-all-closed", () => {
-//   if (process.platform !== "darwin") {
-//     app.quit();
-//   }
-// });
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+let mainWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1400,
     height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+    },
   });
-
-  win.loadFile(path.join(__dirname, "frontend/dist/index.html"));
-
-  win.webContents.openDevTools();
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadFile(
+    path.join(__dirname, "frontend", "dist", "index.html")
+  );
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 1400,
+//     height: 900,
+//   });
+// win.setMenuBarVisibility(false);
+//   win.loadFile(path.join(__dirname, "frontend/dist/index.html"));
+
+//   win.webContents.openDevTools();
+// }
+
+// app.whenReady().then(createWindow);
